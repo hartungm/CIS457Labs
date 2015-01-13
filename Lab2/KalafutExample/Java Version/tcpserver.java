83a7593d901f34b41130563ee249405da995c98b
Michael Hartung <hartungm@mail.gvsu.edu>
2015-01-13 15:02:10 -0500

    Java Server

---
+++ Lab2/KalafutExample/Java Version/tcpserver.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+class tcpserver {
+	public static void main(String args[]) throws Exception {
+		ServerSocket listenSocket = new ServerSocket(9876);
+		while(true) {
+			Socket connectionSocket = listenSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			String clientMessage = inFromClient.readLine();
+			System.out.println("The client said: " + clientMessage);
+			outToClient.writeBytes(clientMessage + '\n');
+			connectionSocket.close();
+		}
+	}
+}
