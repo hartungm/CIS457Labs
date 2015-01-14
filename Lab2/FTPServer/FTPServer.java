b8214ebc3677f38bf7786333f21803ee39f4ee73
matthewarmand <marmand68@gmail.com>
2015-01-13 22:35:51 -0500

    Added check and exception handling for file not found

--- Lab2/FTPServer/FTPServer.java
+++ Lab2/FTPServer/FTPServer.java
@@ -14,17 +14,24 @@ class FTPServer {
 			
 			// Read file path from client and find the file
 			String serverFilePath = inFromClient.readLine();
-			File serverFile = new File(serverFilePath);
-			FileInputStream fileToClient = new FileInputStream(serverFile);
+			
+            try {
+                File serverFile = new File(serverFilePath);
 
-			// Send file to client 1 byte at a time
-			for (long i = 0; i < serverFile.length(); i++) {
-				outToClient.writeByte(fileToClient.read());
-			}
-            outToClient.writeByte(-1); //signifies end of file
-            
-            //close connection
-			connectionSocket.close();
+                FileInputStream fileToClient = new FileInputStream(serverFile);
+
+			    // Send file to client 1 byte at a time
+			    for (long i = 0; i < serverFile.length(); i++) {
+				    outToClient.writeByte(fileToClient.read());
+			    }
+                outToClient.writeByte(-1); //signifies end of file
+            } catch (FileNotFoundException f) {
+                //send file not found code to client
+                outToClient.writeByte(-2);
+            } finally {
+                //close connection
+                connectionSocket.close();
+            }
 		}
 	}
 }
