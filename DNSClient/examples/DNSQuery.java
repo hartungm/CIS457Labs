3dbf29ae17bd7e2b907c320b3d548a26e334eaf3
matthewarmand <marmand68@gmail.com>
2015-02-10 14:26:47 -0500

    Moar Example Codegit add .!

--- DNSClient/examples/DNSQuery.java
+++ DNSClient/examples/DNSQuery.java
@@ -1,2 +1,19 @@
 import java.io.*;
 import java.net.*;
+import java.util.Random;
+
+class DNSQuery {
+    public static void main(String args[]) throws Exception {
+        DatagramSocket clientSocket = new DatagramSocket();
+        clientSocket.setSoTimeout(5000);
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a domain name: ");
+        String domain = inFromUser.readLine();
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream d = new DataOutputStream(b);
+        Random r = new Random();
+        short id = (short)r.nextInt();
+        short flags = 0;
+        flags |= (1<<8);
+    }
+}
